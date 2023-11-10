3ce7301b6cd59f997557efc51ff9bcff4856284c
Guilherme Rosa <guilherme.silveira@betha.com.br>
2023-11-10 16:18:41 -0300

    Redirect mapa inexistente

--- app/routes/ver.$slug.tsx
+++ app/routes/ver.$slug.tsx
@@ -33,9 +33,9 @@ export async function loader({ request, params }: LoaderArgs) {
   //   failureRedirect: '/entrar',
   // });
 
-  //let mapa = await pegarMapaPeloSlug(params.slug);
+  let mapa = await pegarMapaPeloSlug(params.slug);
 
-  //if (mapa === null) return redirect('/');
+  if (mapa === null) return redirect('/');
 
   return {};
 }
